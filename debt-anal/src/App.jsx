4cc1981c1b43b93bca369f5310bc04d0d83e3c7a
destiny-tracer <spiritrident@gmail.com>
2025-11-13 17:11:48 +0530

    Create App.jsx

---
+++ debt-anal/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Debt Health Analyzer (React Version)</h1>
+      <p>This is the starting point. Components will be added here.</p>
+    </div>
+  );
+}
